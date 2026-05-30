0507adec1872dd7ad7b0b382c87e96db0e0ec4fa
Jonathan-C-L <jle16@nait.ca>
2026-05-29 18:33:48 -0600

    file to allow for global type declarations (specifically chrome)

---
+++ client/global.d.ts
@@ -0,0 +1,3 @@
+// This file allows for global type declarations - specifically used for the Chrome API types in this project. 
+// By including the reference to "chrome" types, we can use the Chrome API in our TypeScript code without type errors.
+/// <reference types="chrome" />
