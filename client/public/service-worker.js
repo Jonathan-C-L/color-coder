9179dfcaa30b584e1d7bc37f59f629a038adea69
Jonathan-C-L <jle16@nait.ca>
2026-05-29 18:34:50 -0600

    notes to explain service worker function

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,3 +1,4 @@
+// Open and close the side panel when the extension icon clicked
 chrome.sidePanel
     .setPanelBehavior({openPanelOnActionClick: true})
     .catch((error) => console.error(error));
