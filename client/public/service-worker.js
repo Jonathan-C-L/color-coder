2ca3b536cc69059ac9fbbbc25ec9d5f5cc7a7fab
Jonathan-C-L <jle16@nait.ca>
2026-06-02 19:03:13 -0600

    properly separated component and hook from initial starter project - will populate now to fit specific needs

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,4 +1,4 @@
-// Open and close the side panel when the extension icon clicked
+// Open and close the side panel when the extension icon clicked (Global implementation)
 chrome.sidePanel
     .setPanelBehavior({openPanelOnActionClick: true})
     .catch((error) => console.error(error));
---
+++ client/src/components/Toggle.tsx
@@ -0,0 +1,11 @@
+export const Toggle = () => {
+    return (
+        <div>
+            <div className="hk-toggle hk-tg-2">
+            <span>Color Coder</span>
+            <input type="checkbox" id="tg2"/>
+            <label htmlFor="tg2"></label>
+            </div>
+        </div>
+    );
+};
