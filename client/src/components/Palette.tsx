cc36335d95468930626e2dc52c579adb9a0f526d
Jonathan-C-L <jle16@nait.ca>
2026-08-07 23:40:39 -0600

    updated pdf export format to be more visually appealing

--- client/src/components/Palette.tsx
+++ client/src/components/Palette.tsx
@@ -9,7 +9,8 @@ export const Palette = ({ paletteColors }: PaletteProps) => {
     return (
         <div id="palette" className="display">
             <div className="palette-header">
-                <div className="palette-header__label">Palette ({paletteColors.length}/{config.MAX_PALETTE_SIZE} Items)</div>          
+                <div className="palette-header__label">Palette</div>
+                <div className="palette-header__label">{paletteColors.length}/{config.MAX_PALETTE_SIZE}</div>     
             </div>
 
             {/* Conditionally show palette empty message */}
--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    MAX_PALETTE_SIZE: 8,
+    MAX_PALETTE_SIZE: 10,
     HISTORY_SWATCH_SIZE: 24,
     ICON_SIZE: 16
 }
